380a043377e7324a3cefbfc18ea3acebbc3ac933
Reema Khaseeb <reemafkhaseeb@gmail.com>
2023-08-08 11:13:32 +0300

    create Passenger class

---
+++ AirportTicketBooking/Passenger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTicketBooking
+{
+    public class Passenger
+    {
+        public string PassengerName { get; set; }
+        public string PassportNumber { get; set; }
+    }
+}
