a8fea48b46771d12c59fe65e23586554c042d24a
Reema Khaseeb <reemafkhaseeb@gmail.com>
2023-08-20 16:26:56 +0300

    Create BookingService class

---
+++ AirportTicketBooking/Services/BookingService.cs
@@ -0,0 +1,17 @@
+﻿using Airport_Ticket_Booking3.Models;
+
+namespace AirportTicketBooking.Services
+{
+    internal class BookingService
+    {
+        //public List<Booking> Bookings { get; private set; }
+        private static List<Booking> Bookings { get; set; }
+
+        public BookingService(List<Booking>? newBooking = null)
+        {
+            Bookings = newBooking ?? new List<Booking>();
+        }
+
+        public static List<Booking> GetAllBookings() => Bookings;
+    }
+}
